45cd8ca4e15469edcd6ccfd3405994196559d8fe
malekazmy00 <malekazmy00@gmail.com>
2026-08-05 22:08:07 +0300

    Add import for flutter material package

--- lib/features/admin/presentation/admin_home_screen.dart
+++ lib/features/admin/presentation/admin_home_screen.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../core/theme/app_theme.dart';
